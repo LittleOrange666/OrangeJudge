bf4b648a15b1964f6d453281b55063c68241a04c
LittleOrange666 <92164979+LittleOrange666@users.noreply.github.com>
2025-01-09 00:17:42 +0800

    using judger v1.1.1

--- tools/autoinit.sh
+++ tools/autoinit.sh
@@ -5,7 +5,8 @@ sudo apt-get -y install python3-pip
 sudo apt-get -y install redis
 sudo apt-get -y install texlive-latex-generic
 sudo apt-get -y install texlive-xetex
+sudo apt-get -y install docker
+sudo apt-get -y install docker-compose
 sudo pip3 install -r tools/requirements.txt
-sudo tools/prepare_judger.sh
 sudo python3 tools/add_ignored.py
 echo ok > tools/inited
--- tools/prepare_judger.sh
+++ tools/prepare_judger.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-sudo apt-get -y install docker
-sudo apt-get -y install docker-compose
 cd ..
 git clone https://github.com/LittleOrange666/OrangeJudge_Judger.git --recursive --depth 1
 cd OrangeJudge_Judger
